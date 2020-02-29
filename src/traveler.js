8c9b665126e9900d3dd7750b0d0d7ebaacb22279
Jeremy Poulter <Jpoulter@gmail.com>
2020-02-28 20:30:19 -0700

    Create traveler class with properties and rough draft of methods

---
+++ src/traveler.js
@@ -0,0 +1,19 @@
+class Traveler extends User {
+  constructor(user, travelerType) {
+    super(user)
+  }
+
+  makeTripRequest() {
+
+  }
+
+  calculateEstimatedCost() {
+
+  }
+
+  calculateTotalSpent() {
+
+  }
+}
+
+export default Traveler;
