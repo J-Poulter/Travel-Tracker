ebf7dfa3e74cf23a80424f6f4ea866bc7c20c98b
Jeremy Poulter <Jpoulter@gmail.com>
2020-02-29 00:08:55 -0700

    Download background images to images directory and properly link them in html and css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
+// import domUpdates from './domUpdates';
+// import User from './user';
+// import TravelAgent from './travelAgent';
+// import Traveler from './traveler';
+// import Trip from './trip';
+// import Destination from './destination';
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+import './images/LoginBackground.jpg';
+import './images/TravelBackground.jpg';
+
+// var currentUser, destinationsData, tripsData;
+let dbd = 'a'
+// let logInButton = document.querySelector('#sub-button');
+// let usernameInput = document.querySelector('#usernameInput');
+// let passwordInput = document.querySelector('#passwordInput');
+
+
+// logInButton.addEventListener('click', user.logIn(usernameInput.value, passwordInput.value))
+// window.addEventListener('onload', obtainData);
+
+// async function obtainData() {
+//   const res = await fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
+//     let destinationsData = await res.json();
+//   // const res2 = await fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+//   //   const tripsData = await res2.json();
+// }
 
 console.log('This is the JavaScript entry file - your code begins here.');
