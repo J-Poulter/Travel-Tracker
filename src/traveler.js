cddc34b8332acc1d6df3838448ac1e70ad6640b5
Jeremy Poulter <Jpoulter@gmail.com>
2020-02-29 19:18:47 -0700

    Create some of the methods in traveler class

--- src/traveler.js
+++ src/traveler.js
@@ -1,17 +1,21 @@
 import User from './user';
 
-
 class Traveler extends User {
-  constructor(user, usersTrips) {
+  constructor(user, tripsData) {
     super(user)
-    this.usersTrips = usersTrips
+    this.usersTrips = tripsData.filter(usertrip => usertrip.userID === this.id);
   }
 
-  makeTripRequest() {
-    let request = {
-
+  async makeTripRequest(thisTrip) {
+    let res = await fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }, body: JSON.stringify(thisTrip),
+    })
+      let req = await res.json();
+      console.log(req)
     }
-  }
 
   calculateTotalSpent() {
 
