6545e3c99cabb77604ccc85f9438f5f91a5b7789
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-01 19:45:53 -0700

    Create domUpdates methods that populates the travelers webpages

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -20,21 +20,92 @@ let domUpdates = {
   populateTravelerPage(usersId) {
     $('main').html('');
     $('body').removeClass('loginBody').addClass('travelerBody')
+    $('main').addClass('destinations-display')
+    this.createDestinationCards();
+
     let thisUser = travelersData.find(user => user.id == usersId)
-    currentUser = new Traveler(thisUser, tripsData)
+    currentUser = new Traveler(thisUser, tripsData, destinationsData)
+    let spent = currentUser.calculateTotalSpent()
     console.log(currentUser)
+    console.log(spent)
+    let trips = currentUser.displayTrips()
+    console.log(trips)
+    let pending = currentUser.displayRequests();
+    console.log(pending)
+
+    this.createTravelerNavBar()
     // currentUser = new User()
   },
 
   populateAgentPage() {
     $('main').html('');
     $('body').removeClass('loginBody').addClass('agencyBody')
-    console.log(travelersData)
+    currentUser = new TravelAgent({"id":0,"name":'Franky',"travelerType":'Agent'}, tripsData, destinationsData)
+    console.log(currentUser)
+    //FIND PROPER PLACE FOR FUNCTION BELOW
+    let income = currentUser.calculateYearsIncome()
+    console.log(income)
+    let requests = currentUser.displayRequests();
+    console.log(requests)
+
   },
 
   loginErrorDisplay() {
-    return
+    $('.login-container').append("<p class='warning-text'>Login Failed: Incorrect Username or Password</p>")
+    $('.login-fields').val('')
+  },
+
+  createTravelerNavBar() {
+    $('body').prepend(`
+      <nav class='traveler-nav'>Welcome ${currentUser.name}!
+      <div class='traveler-button-container'>
+      <button class='traveler-button traveler-button2'>All Destinations</button>
+      <button class='traveler-button traveler-button1'>My Trips Information</button>
+      </div></nav>
+    `)
+    $('.traveler-button1').click(() => this.createTravelerTrips())
+    $('.traveler-button2').click(() => this.createDestinationCards())
+  },
+
+  createDestinationCards() {
+    $('main').html('')
+    destinationsData.forEach(destination => {
+      $('main').prepend(
+        `<div id='${destination.id}' class='card'>
+        <header data-id='${destination.id}'>
+        </header>
+        <span data-id='${destination.id}' class='destination-name'>${destination.destination}</span>
+        <img data-id='${destination.id}' tabindex='0' class='card-picture' src='${destination.image}' alt='${destination.alt}'>
+        <p class="card-cost-info">Flight Cost per Person: $${destination.estimatedFlightCostPerPerson}</p>
+        <p class="card-cost-info">Lodging Cost per Day: $${destination.estimatedLodgingCostPerDay}</p>
+        </div>`)
+    })
+  },
+
+  createTravelerTrips() {
+    $('main').html('')
+    currentUser.usersTrips.forEach(trip => {
+      let curTrip = new Trip(trip, destinationsData)
+      console.log(curTrip)
+      let tripDestination = curTrip.returnDestinationDetails()
+      console.log(tripDestination)
+      console.log(curTrip)
+      $('main').prepend(
+        `<div class='travelers-trips card'>
+        <img tabindex='0' class='card-picture' src='${tripDestination.image}' alt='${tripDestination.alt}'>
+        <p class='traveler-trips-info'><span class='traveler-trips-span'>Location:</span>  ${tripDestination.destination}</p>
+        <p class='traveler-trips-info'><span class='traveler-trips-span'>Trips Cost:</span>  $${curTrip.calculateEstimatedCost()}</p>
+        <p class='traveler-trips-info'><span class='traveler-trips-span'>Status:</span>  ${curTrip.status}</p>
+        <p class='traveler-trips-info'><span class='traveler-trips-span'>Starting Date:</span>  ${curTrip.date}</p>
+        <p class='traveler-trips-info'><span class='traveler-trips-span'># of Travelers:</span>  ${curTrip.travelers}</p>
+        </div>
+        `)
+    })
+    $('main').prepend(`<div class='total-spent'>You have spent $${currentUser.calculateTotalSpent()} in 2020</div>`)
+
   }
+
+
 }
 
 
