6dbb7a1023e0d02bd300e86996407a03192e96da
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-01 19:44:09 -0700

    Fill out couple of the missing methods on class

--- src/travelAgent.js
+++ src/travelAgent.js
@@ -1,32 +1,68 @@
 import User from './user';
+import Trip from './trip';
+var moment = require('moment');
+//moment().format(); Correct format??
+moment().format('YYYY MMMM Do');
 
 class TravelAgent extends User {
-  constructor(user) {
-    super(user)
+  constructor(user, tripsData, destinationsData) {
+    super(user, tripsData, destinationsData)
   }
 
   searchUserDetails() {
 
   }
 
-  approveTripRequest() {
-
+  async approveOrDenyTripRequest(tripID, determination) { //NEEDS TO BE AN OBJ WITH ID AND APPROVED/DENIED STATUS ONLY//
+    let thisTrip = {
+      id: tripID,
+      status: determination
+    }
+    let res = await fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/updateTrip", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }, body: JSON.stringify(thisTrip),
+    })
+    let req = await res.json();
+    console.log(req)
   }
 
-  denyTripRequest() {
-    
-  }
+  // denyTripRequest() { //NEEDS TO BE AN OBJ WITH ID AND APPROVED STATUS ONLY//
+  //   let res = await fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/updateTrip", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }, body: JSON.stringify(thisTrip),
+  //   })
+  //   let req = await res.json();
+  //   console.log(req)
+  // }
 
   calculateYearsIncome() {
-
+    let thisYearsTrips = this.tripsData.filter(trip => trip.status === 'approved' && trip.date.includes(2020))
+    let totalIncome = thisYearsTrips.reduce((totalSpent, curTrip) => {
+      let trip = new Trip(curTrip, this.destinationsData);
+      totalSpent += trip.calculateEstimatedCost()
+      return totalSpent;
+    }, 0) / 1.1 * 0.1
+    return Math.floor(totalIncome)
   }
 
   displayTodaysTrips() {
 
   }
 
-  deleteUpcomingTrip() {
-
+  async deleteUpcomingTrip(tripID) {
+    let thisTrip = {"id": tripID}
+    let res = fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }, body: JSON.stringify(thisTrip)
+    })
+    let req = await res.json()
+    console.log(req)
   }
 }
 
