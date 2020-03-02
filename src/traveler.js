2cb5436f3d9d122cf2c98dc05d00dd0c10ffa944
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-01 19:43:32 -0700

    Add property of dataset to minimize method parameters

--- src/traveler.js
+++ src/traveler.js
@@ -1,12 +1,13 @@
 import User from './user';
+import Trip from './trip';
 
 class Traveler extends User {
-  constructor(user, tripsData) {
-    super(user)
-    this.usersTrips = tripsData.filter(usertrip => usertrip.userID === this.id);
+  constructor(user, tripsData, destinationsData) {
+    super(user, tripsData, destinationsData)
+    this.usersTrips = this.tripsData.filter(usertrip => usertrip.userID === this.id);
   }
 
-  async makeTripRequest(thisTrip) {
+  async makeTripRequest(thisTrip) { //NEEDS TO BE FORMAT OF TRIP OBJ//
     let res = await fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips", {
       method: "POST",
       headers: {
@@ -18,7 +19,24 @@ class Traveler extends User {
     }
 
   calculateTotalSpent() {
+    let approvedTrips = this.usersTrips.filter(trip => trip.status === 'approved' && trip.date.includes('2020'));
+    return approvedTrips.reduce((total, curTrip) => {
+      let trip = new Trip(curTrip, this.destinationsData);
+      total += trip.calculateEstimatedCost()
+      return total;
+    }, 0)
+  }
 
+  displayPreviousTrips() {
+    // return this.usersTrips.filter(trip => trip.date < Date.Now())
+  }
+
+  displayPresentTrips() {
+    // return this.usersTrips.filter(trip => trip.date < Date.Now < trip.date+duration())
+  }
+
+  displayUpcomingTrips() {
+    // return this.usersTrips.filter(trip => trip.date > Date.Now())
   }
 }
 
